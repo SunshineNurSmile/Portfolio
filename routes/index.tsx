8a90b3b750c93fdcd3cd1e20e8d88e92e33d4865
SunshineNurSmile <yuanyiw@bu.edu>
2022-07-03 02:34:05 -0400

    Testing Deno Deploy

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,8 +4,8 @@ import { tw } from "@twind";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <p class={tw`flex justify-center`}>
+    <div class={tw`flex bg-gray-800 mx-auto h-screen w-screen justify-center items-center`}>
+      <p class={tw`text-white`}>
         This is going to be my portfolio site.
       </p>
     </div>
