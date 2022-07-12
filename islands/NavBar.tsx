e7db3c67902dc4cf5209241225c188b96176f271
Yuanyi Wang <yuanyiw@bu.edu>
2022-07-12 16:54:37 -0400

    Refactored show menu function

--- islands/NavBar.tsx
+++ islands/NavBar.tsx
@@ -7,6 +7,14 @@ const jumpHome = () => {
     window.location.href = "/";
 };
 
+const showMenu = () => {
+    if (document.getElementById("menu")!.classList.contains("hidden")) {
+        document.getElementById("menu")!.classList.remove("hidden");
+    } else {
+        document.getElementById("menu")!.classList.add("hidden");
+    }
+};
+
 const NavBar = () => {
     return (
         <div
@@ -42,22 +50,7 @@ const NavBar = () => {
             <div class={tw`flex items-center md:hidden mr-6`}>
                 <svg
                     class={tw`h-6 w-6 hover:cursor-pointer`}
-                    onClick={() => {
-                        if (
-                            document
-                                .getElementById("menu")!
-                                .classList.contains("hidden")
-                        ) {
-                            document
-                                .getElementById("menu")!
-                                .classList.remove("hidden");
-                        }
-                        else {
-                            document
-                            .getElementById("menu")!
-                            .classList.add("hidden");
-                        }
-                    }}
+                    onClick={showMenu}
                 >
                     <path
                         d="M3 6C3 5.44772 3.44772 5 4 5H20C20.5523 5 21 5.44772 21 6 21 6.55228 20.5523 7 20 7H4C3.44772 7 3 6.55228 3 6zM3 12C3 11.4477 3.44772 11 4 11H20C20.5523 11 21 11.4477 21 12 21 12.5523 20.5523 13 20 13H4C3.44772 13 3 12.5523 3 12zM4 17C3.44772 17 3 17.4477 3 18 3 18.5523 3.44772 19 4 19H20C20.5523 19 21 18.5523 21 18 21 17.4477 20.5523 17 20 17H4z"
